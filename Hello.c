95528941db851ab562332df5c10f063f596c5709
AbhishekJain18 <97720313+AbhishekJain18@users.noreply.github.com>
2022-06-01 20:45:06 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//global variable
+int I = 11;
+int J;
+
+void Demo(int no)
+{
+    int A = 20; //Local variable
+}
+
+void Hello()
+{
+    int X = 10;
+}
+
+int main()
+{
+    int A = 30;
+    int B;
+    return 0;
+}
